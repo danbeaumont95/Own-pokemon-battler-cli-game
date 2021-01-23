45a25589fa56cda671975233d702a57fb6a3b445
Daniel <danielbeaumont95@hotmail.co.uk>
2021-01-23 15:01:23 +0000

    added battle class and 4 pokemon

--- pokemon-battler.js
+++ pokemon-battler.js
@@ -53,5 +53,109 @@ class Trainer {
 }
 
 class Battle {
-    
+    constructor(trainer1, trainer2, pokemon1, pokemon2) {
+        this.trainer1 = trainer1;
+        this.trainer2 = trainer2;
+        this.pokemon1 = pokemon1;
+        this.pokemon2 = pokemon2;
+        this.turn = pokemon1;
+    }
+    fight() {
+        if (this.turn === this.pokemon1) {
+            this.turn === this.pokemon2;
+            if (this.pokemon1.strongAgainst === this.pokemon2.type) {
+                this.pokemon2.health = this.pokemon2.health - (this.pokemon1.attackDamage * 1.25)
+                if (this.pokemon2.health <= 0) {
+                    return `Goodbye, you have been defeated`
+                }
+                else {
+                    return `I will get you next time`
+                }
+            }
+            else if (this.pokemon1.weakAgainst === this.pokemon2.type) {
+                this.pokemon2.health = this.pokemon2.health - (this.pokemon1.attackDamage * 0.75)
+                if (this.pokemon2.health <= 0) {
+                    return `Goodbye, you have been defeated`
+                }
+                else {
+                    return `I will get you next time`
+                }
+            }
+            else {
+                this.pokemon2.health = this.pokemon2.health - this.pokemon1.attackDamage;
+                if(this.pokemon2.health <= 0) {
+                    return "Goodbye, I have beaten you"
+                }
+                else {
+                    return "I will get you next time"
+                }
+            }
+        }
+        else if (this.turn === this.pokemon2) {
+            if(this.pokemon2.strongAgainst === this.pokemon1.type) {
+                this.pokemon1.health = this.pokemon1.health - (this.pokemon2.attack * 1.25)
+            
+            if(this.pokemon1.health <= 0) {
+                return "Goodbye, I have beaten you"
+            }
+            else {
+                return "I will get you next time"
+            }
+        }
+        else if(this.pokemon2.weakAgainst === this.pokemon1.type) {
+            this.pokemon1.health = this.pokemon1.health - (this.pokemon2.attack * 0.75)
+            if(this.pokemon1.health <=0) {
+                return "Goodbye, I have beaten you"
+            }
+            else {
+                return "I will get you next time"
+            }
+        }
+        else {
+            this.pokemon1.health = this.pokemon1.health - this.pokemon2.attack
+            if(this.pokemon1.health <= 0) {
+                return "Goodbye, I have beaten you"
+            }
+            else {
+                return "I will get you next time"
+            }
+        }
+        }
+    }
 }
+const Charizard = new Pokemon (
+'Charizard',
+100,
+20,
+'Raaaaawr',
+'Flamethrower',
+'Fire'
+)
+
+const Blastoise = new Pokemon (
+    'Blastoise',
+    100,
+    20,
+    'Squirttt',
+    'Hydro Cannon',
+    'water'
+)
+
+const Venusaur = new Pokemon (
+    'Venusaur',
+    100,
+    20,
+    'Venusss',
+    'Ripple',
+    'grass'
+)
+
+const Pikachu = new Pokemon (
+    'Pikachu',
+    100,
+    15,
+    'Pika pika',
+    'Lightning Bolt',
+    'normal'
+)
+
