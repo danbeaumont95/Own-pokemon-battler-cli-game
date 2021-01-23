958b9b5b723bd012dc4113594652e824b4ca0473
Daniel <danielbeaumont95@hotmail.co.uk>
2021-01-23 12:41:56 +0000

    set up pokemon class

---
+++ pokemon-battler.js
@@ -0,0 +1,44 @@
+class Pokemon {
+    constructor(name, health, attackDamage, sound, move, type) {
+        this.name = name;
+        this.health = health;
+        this.attackDamage = attackDamage;
+        this.sound = sound;
+        this.move = move;
+
+        if (!type) {
+            this.type = 'normal'
+        }
+        else {
+            this.type = type;
+        }
+        if (this.type === 'grass') {
+            this.strength = 'grass';
+            this.strongAgainst = 'water';
+            this.weakAgainst = 'fire';
+        }
+        else if (this.type === 'water') {
+            this.strength = 'water';
+            this.strongAgainst = 'fire';
+            this.weakAgainst = 'grass';
+        }
+        else if (this.type === 'fire') {
+            this.strength = 'fire';
+            this.strongAgainst = 'grass';
+            this.weakAgainst = 'water';
+        }
+        else {
+            this.strength === 'none';
+        }
+    }
+    sound() {
+        return this.sound;
+    }
+    useMoves() {
+        return this.move;
+    }
+}
+
+class Trainer {
+    
+}
